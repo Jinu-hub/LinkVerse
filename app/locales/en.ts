f1a5f5d64d7c653b42597996b088b0b30d1b0a78
Jinwoo's mac air <takefree2013withu@gmail.com>
2025-06-26 19:37:47 +0900

    shadcn components 추가,  locale파일 수정

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -2,8 +2,8 @@ import type { Translation } from "./types";
 
 const en: Translation = {
   home: {
-    title: "Supaplate",
-    subtitle: "It's time to build!",
+    title: "LinkVerse",
+    subtitle: "Build your knowledge base",
   },
   navigation: {
     en: "English",
