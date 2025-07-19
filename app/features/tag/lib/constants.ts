6b6f2308ba6722cb59cbe0a1535a83801e265c08
Jinwoo's mac air <takefree2013withu@gmail.com>
2025-07-19 19:03:13 +0900

    untagged 태그입력 폼 구현

--- app/features/tag/lib/constants.ts
+++ app/features/tag/lib/constants.ts
@@ -28,9 +28,8 @@ const TAG_COLORS = [
     { value: "title", label: "제목" , width: "25%", isSortable: true },
     { value: "url", label: "URL" , width: "20%", isSortable: true },
     { value: "createdAt", label: "생성일" , width: "10%", isSortable: true },
-    /*
     { value: "tags", label: "태그입력" , width: "40%", isSortable: false },
-    { value: " ", label: " " , width: "5%", isSortable: false },*/
+    { value: " ", label: " " , width: "5%", isSortable: false },
   ] as const;
 
 export const CONTENT_TYPE_FIELD_MAP: Record<string, { label: string; key: string }[]> = {
